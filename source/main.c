5d447e99317cbf6f54c058046b2a8a3af6a71cba
Joe Balough <scallopedllama@gmail.com>
2010-01-22 07:38:04 -0500

    changed hello world! confirmed it all runs with no

--- source/main.c
+++ source/main.c
@@ -9,7 +9,14 @@
 int main(void) {
 //---------------------------------------------------------------------------------
 	consoleDemoInit();
-	iprintf("Hello World!");
+	iprintf("           Hello World!\n");
+	iprintf("  When I grow up, I want to be \n    a 2D side-scrolling game!\n\n\n");
+	iprintf("       ________\n");
+	iprintf("              /\n");
+	iprintf("   DR        /\n");
+	iprintf("            /\n");
+	iprintf("           /\n");
+	iprintf("          /_______ engine\n");
 	while(1) {
 		swiWaitForVBlank();
 	}
