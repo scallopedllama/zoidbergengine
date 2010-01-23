08f1338731baca8bb6a5b8caf7c7479b517c4a31
Joe Balough <scallopedllama@gmail.com>
2010-01-23 18:53:27 -0500

    Switched over to a template that uses separate arm7 and arm9 source (instead of using the devkitpro-default-arm7 executable).
    Converted old 0.0.1 release to new source.

--- source/arm9/source/main.cpp
+++ source/arm9/source/main.cpp
@@ -1,14 +1,19 @@
 /*---------------------------------------------------------------------------------
 
-	Basic template code for starting a DS app
+	Simple console print demo
+	-- dovoto
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <stdio.h>
+
 //---------------------------------------------------------------------------------
 int main(void) {
 //---------------------------------------------------------------------------------
-	consoleDemoInit();
+	touchPosition touch;
+
+	consoleDemoInit();  //setup the sub screen for printing
+
 	iprintf("           Hello World!\n");
 	iprintf("  When I grow up, I want to be \n    a 2D side-scrolling game!\n\n\n");
 	iprintf("       ________\n");
@@ -17,8 +22,15 @@ int main(void) {
 	iprintf("            /\n");
 	iprintf("           /\n");
 	iprintf("          /_______ engine\n");
+
 	while(1) {
+
+		touchRead(&touch);
+		iprintf("\x1b[10;0HTouch x = %04i, %04i\n", touch.rawx, touch.px);
+		iprintf("Touch y = %04i, %04i\n", touch.rawy, touch.py);
+
 		swiWaitForVBlank();
 	}
 
+	return 0;
 }
