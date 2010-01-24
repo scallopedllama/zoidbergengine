a79f97af1166b680734c6878773354db9cee68ae
Joe Balough <scallopedllama@gmail.com>
2010-01-23 22:12:53 -0500

    Trivial change to the main.cpp file

--- source/arm9/source/main.cpp
+++ source/arm9/source/main.cpp
@@ -18,8 +18,8 @@ int main(void) {
 	iprintf("  When I grow up, I want to be \n    a 2D side-scrolling game!\n\n\n");
 	iprintf("       ________\n");
 	iprintf("              /\n");
-	iprintf("   DR        /\n");
-	iprintf("            /\n");
+	iprintf(" |\\ |>       /\n");
+	iprintf(" |/ |\\      /\n");
 	iprintf("           /\n");
 	iprintf("          /_______ engine\n");
 
