fb02564c8e350c8f277c54e063bd6c4d80717cb4
Joe Balough <scallopedllama@gmail.com>
2010-04-03 07:09:00 -0400

    Lots of untested work done on new cli creator that will hopefully work in windows

---
+++ tools/cliCreator/cliCreator.cpp
@@ -0,0 +1,289 @@
+/**
+ * @file zbeParser.cpp
+ *
+ * @brief A tool to parse a zbe description xml file
+ *
+ * This file is the new cliCreator and will generate an assets.zbe file based on the
+ * following xml structure:
+ *
+ * <zbe>
+ * </zbe>
+ *
+ * into a binary file to be dropped into an assets file that will be properly loaded by the
+ * zoidberg engine.
+ *
+ * @see asset.h
+ * @author Joe Balough
+ */
+
+/**
+ *  Copyright (c) 2010 zoidberg engine
+ *
+ *  This file is part of the zoidberg engine.
+ *
+ *  The zoidberg engine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The zoidberg engine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the zoidberg engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <vector>
+#include <string>
+#include "bin/lib/tinyxml/tinyxml.h"
+
+#define TIXML_USE_STL
+
+/**
+ *   Define the zbe verison here!!
+ */
+#define ZBE_VERSION 1
+
+using namespace std;
+
+void fwrite8(uint8_t val, FILE *file);
+void fwrite16(uint16_t val, FILE *file);
+void fwrite32(uint32_t val, FILE *file);
+int getIntAttr(TiXmlElement *elem, string attr);
+string getStrAttr(TiXmlElement *elem, string attr);
+
+int main(int argc, char **argv)
+{
+	if(argc < 3)
+	{
+		fprintf(stderr, "Parses an asset xml file and outputs its proper binary data.\n");
+		fprintf(stderr, "Usage: %s [input filename] [output filename]\n", argv[0]);
+		if(argv[1] == "h")
+		{
+			fprintf(stderr, "Expects XML file to be in the following form:\n");
+			// TODO: fill this in
+		}
+		return EXIT_FAILURE;
+	}
+
+	// Attempt to load up argv[1] as a tinyxml document
+	TiXmlDocument input(argv[1]);
+	if (!input.LoadFile())
+	{
+		fprintf(stderr, "Failed to parse file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	// Attempt to load up the output file
+	FILE *output = fopen(argv[2], "wb");
+	if (!output)
+	{
+		fprintf(stderr, "Failed to open %s for output.\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	// Get the root node
+	TiXmlElement *zbeXML = input.RootElement();
+	
+	
+	/**
+	 *    ZBE GAME DATA
+	 */
+	 
+	// Version Number
+	fwrite16(ZBE_VERSION, output);
+	
+	// Total # assets. There is no way of knowing how may assets we will end up
+	// with, so write a 32 bit int 0 to the file and remember the position.
+	// When all parsing is done, we will return to that point in the file and
+	// write the actual value in.
+	fpos_t totalAssetsPos;
+	uint32_t totalAssets = 0;
+	fgetpos(output, &totalAssetsPos);
+	fwrite32(0, output);
+	
+	
+	
+	/**
+	 *   GRAPHICAL ASSETS
+	 */
+	
+	// Total # gfx. Do the same like total number assets
+	fpos_t totalGfxPos;
+	uint32_t totalGfx = 0;
+	fgetpos(output, &totalGfxPos);
+	fwrite32(0, output);
+	
+	
+	// For all the graphics in the XML file
+	TiXmlElement *graphicsXML = zbeXML->FirstChildElement("bin")->FirstChildElement("graphics");
+	TiXmlElement *gfxXML = graphicsXML->FirstChildElement("gfx");
+	while (gfxXML)
+	{
+		// Increment total gfx counter
+		++totalGfx;
+		
+		// Get all the needed attributes
+		string thisBin = getStrAttr(gfxXML, "bin");
+		int w = getIntAttr(gfxXML, "w");
+		int h = getIntAttr(gfxXML, "h");
+		int t = getIntAttr(gfxXML, "top");
+		int l = getIntAttr(gfxXML, "left");
+		
+		// Copy it into the zbe file
+		fwrite8((uint8_t) w, output);
+		fwrite8((uint8_t) h, output);
+		fwrite8((uint8_t) t, output);
+		fwrite8((uint8_t) l, output);
+		// The length is unknown right now, it'll be counted in the copy op and returned
+		// so we'll return here after that copy is done
+		fpos_t lenPos;
+		fgetpos(output, &lenPos);
+		fwrite16(0, output);
+		uint16_t len = appendData(output, thisBin);
+		
+		// Now we have the length, so go back and write it down
+		fsetpos(output, &lenPos);
+		fwrite16((uint16_t) len, output);
+		fseek(output, 0, SEEK_END);
+		
+		// Get the next sibling
+		gfxXML = gfxXML->NextSiblingElement("gfx");
+	}
+	// Now that the total number of gfx are known, go back and write that down
+	fsetpos(output, &totalGfxPos);
+	fwrite32(totalGfx, output);
+	fseek(output, 0, SEEK_END);
+	totalAssets += totalGfx;
+	
+	
+	// Total # pal. Do the same like total number assets
+	fpos_t totalPalPos;
+	uint32_t totalPal = 0;
+	fgetpos(output, &totalPalPos);
+	fwrite32(0, output);
+	
+	// For all the palettes in the XML file
+	TiXmlElement *palettesXML = zbeXML->FirstChildElement("bin")->FirstChildElement("palettes");
+	TiXmlElement *palXML = palettesXML->FirstChildElement("palette");
+	while (palXML)
+	{
+		// Increment total gfx counter
+		++totalPal;
+		
+		// Get all the needed attributes
+		string thisBin = getStrAttr(gfxXML, "bin");
+
+		// The length is unknown right now, it'll be counted in the copy op and returned
+		// so we'll return here after that copy is done
+		fpos_t lenPos;
+		fgetpos(output, &lenPos);
+		fwrite16(0, output);
+		uint16_t len = appendData(output, thisBin);
+		
+		// Now we have the length, so go back and write it down
+		fsetpos(output, &lenPos);
+		fwrite16((uint16_t) len, output);
+		fseek(output, 0, SEEK_END);
+		
+		// Get the next sibling
+		gfxXML = gfxXML->NextSiblingElement("palette");
+	}
+	// Now that the total number of palettes are known, go back and write that down
+	fsetpos(output, &totalPalPos);
+	fwrite32(totalPal, output);
+	fseek(output, 0, SEEK_END);
+	totalAssets += totalPal;
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	// Now that the total number of assets are known, go back and write that down
+	fsetpos(output, &totalAssetsPos);
+	fwrite32(totalAssets, output);
+	fseek(output, 0, SEEK_END);
+	
+	// Close the output file and we're done!
+	fclose(output);
+
+	return 0;
+}
+
+void fwrite32(uint32_t val, FILE *file)
+{
+	if (fwrite(&val, sizeof(uint32_t), 1, file) != 1)
+	{
+		fprintf(stderr, "error writing uint32 value %ld to file\n", (long int) val);
+	}
+}
+
+void fwrite16(uint16_t val, FILE *file)
+{
+	if (fwrite(&val, sizeof(uint16_t), 1, file) != 1)
+	{
+		fprintf(stderr, "error writing uint16 value %ld to file\n", (long int) val);
+	}
+}
+
+void fwrite8(uint8_t val, FILE *file)
+{
+	if (fwrite(&val, sizeof(uint8_t), 1, file) != 1)
+	{
+		fprintf(stderr, "error writing uint8 value %d to file\n", (int) val);
+	}
+}
+
+int getIntAttr(TiXmlElement *elem, string attr)
+{
+	int toReturn = -1;
+	if (elem->QueryIntAttribute(attr, &toReturn) == TIXML_WRONG_TYPE)
+	{
+		fprintf(stderr, "Error getting int value of attribute %s\n", attr.c_str());
+	}
+	return toReturn;
+}
+
+string getStrAttr(TiXmlElement *elem, string attr)
+{
+	string toReturn;
+	if (elem->QueryStringAttribute(attr, &toReturn) == TIXML_WRONG_TYPE)
+	{
+		fprintf(stderr, "Error getting string value of attribute %s\n", attr.c_str());
+	}
+}
+
+uint16_t appendData(FILE *output, string inFile)
+{
+	FILE *input = fopen(inFile.c_str(), "rb");
+	uint8_t byte;
+	uint16_t bytes = 0;
+	// fread returns the number of elements read. if file ended, should be 0
+	while ( fread(&byte, sizeof(uint8_t), 1, input) )
+	{
+		++bytes;
+		fwrite8(byte, output);
+	}
+	// That while could cancel because of a bad read, let's check for that
+	if (ferror(input))
+		fprintf(stderr, "Error reading from binary file %s\n", inFile.c_str());
+	
+	// Don't forget to close that file
+	fclose(input);
+	
+	// Return the length of the data
+	return bytes;
+}
+	
