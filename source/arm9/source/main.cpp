ee8d2cb2e2c6186690976c6ef27c4a057c5082cd
Joe Balough <scallopedllama@gmail.com>
2010-04-16 02:51:28 -0400

    main cleanup. removed all the old background stuff that has been disabled for quite some time now. zbe's going to use tile-based backgrounds anyway so this stuff was all pointless anyway

--- source/arm9/source/main.cpp
+++ source/arm9/source/main.cpp
@@ -14,16 +14,6 @@
 
 #include "game.h"
 
-// Backgrounds
-#include "starField.h"
-#include "planet.h"
-#include "splash.h"
-// Sounds
-#include "soundbank.h"
-#include "soundbank_bin.h"
-
-// Select a low priority DMA channel to perform our background copying.
-static const int DMA_CHANNEL = 3;
 
 void initVideo()
 {
@@ -54,100 +44,12 @@ void initVideo()
 	consoleDemoInit();
 }
 
-void initBackgrounds()
-{
-    /*  Set up affine background 3 on main as a 16-bit color background. */
-    REG_BG3CNT = BG_BMP16_256x256 |
-                 BG_BMP_BASE(0) | // The starting place in memory
-                 BG_PRIORITY(3); // A low priority
-
-    /*  Set the affine transformation matrix for the main screen background 3
-     *  to be the identity matrix.
-     */
-    REG_BG3PA = 1 << 8;
-    REG_BG3PB = 0;
-    REG_BG3PC = 0;
-    REG_BG3PD = 1 << 8;
-
-    /*  Place main screen background 3 at the origin (upper left of the
-     *  screen).
-     */
-    REG_BG3X = 0;
-    REG_BG3Y = 0;
-
-    /*  Set up affine background 2 on main as a 16-bit color background. */
-    REG_BG2CNT = BG_BMP16_128x128 |
-                 BG_BMP_BASE(8) | // The starting place in memory
-                 BG_PRIORITY(2);  // A higher priority
-
-    /*  Set the affine transformation matrix for the main screen background 3
-     *  to be the identity matrix.
-     */
-    REG_BG2PA = 1 << 8;
-    REG_BG2PB = 0;
-    REG_BG2PC = 0;
-    REG_BG2PD = 1 << 8;
-
-    /*  Place main screen background 2 in an interesting place. */
-    REG_BG2X = -(SCREEN_WIDTH / 2 - 32) << 8;
-    REG_BG2Y = -32 << 8;
-
-    /*  Set up affine background 3 on the sub screen as a 16-bit color
-     *  background.
-     */
-    REG_BG3CNT_SUB = BG_BMP16_256x256 |
-                     BG_BMP_BASE(0) | // The starting place in memory
-                     BG_PRIORITY(3); // A low priority
-
-    /*  Set the affine transformation matrix for the sub screen background 3
-     *  to be the identity matrix.
-     */
-    REG_BG3PA_SUB = 1 << 8;
-    REG_BG3PB_SUB = 0;
-    REG_BG3PC_SUB = 0;
-    REG_BG3PD_SUB = 1 << 8;
-
-    /*
-     *  Place main screen background 3 at the origin (upper left of the screen)
-     */
-    REG_BG3X_SUB = 0;
-    REG_BG3Y_SUB = 0;
-}
-
-void displayStarField()
-{
-    dmaCopyHalfWords(DMA_CHANNEL,
-                     starFieldBitmap, /* This variable is generated for us by
-                                       * grit. */
-                     (uint16 *)BG_BMP_RAM(0), /* Our address for main
-                                               * background 3 */
-                     starFieldBitmapLen);
-}
-
-void displayPlanet()
-{
-    dmaCopyHalfWords(DMA_CHANNEL,
-                     planetBitmap, /* This variable is generated for us by
-                                    * grit. */
-                     (uint16 *)BG_BMP_RAM(8), /* Our address for main
-                                               * background 2 */
-                     planetBitmapLen);
-}
-
 int main()
 {
 	initVideo();
-	//initBackgrounds();
-
-	// Initialize maxmod using the memory based soundbank set up.
-	//mmInitDefaultMem((mm_addr)soundbank_bin);
 
 	// Initialize libfat
 	fatInitDefault();
-
-	// Display the backgrounds
-	//displayStarField();
-	//displayPlanet();
 
 	// make a game
 	game g((char *) "/assets.zbe");
