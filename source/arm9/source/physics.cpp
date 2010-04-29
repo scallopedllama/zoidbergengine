5b5a4a458e51909f6b7876a555a1565d84009695
Joe Balough <scallopedllama@gmail.com>
2010-04-29 13:57:01 -0400

    nothing really

--- source/arm9/source/physics.cpp
+++ source/arm9/source/physics.cpp
@@ -4,7 +4,7 @@ using namespace decapod;
 
 
 /* (y - w/2)       (y + w/2)
-*       |-----w-----| 
+*       |-----w-----|
 *
 *	    +-----------+  --- (x + h/2)
 *       |           |   |
@@ -18,7 +18,7 @@ using namespace decapod;
 /*
 bool decapod :: intersection( object sprite1,  object sprite2)
 {
-	// if its the same object then ignore 
+	// if its the same object then ignore
 	if(&sprite1 == &sprite2) return false;
 	// Create the bounding boxes for the sprites
 	int s1top, s1bottom, s1left, s1right;
@@ -36,7 +36,7 @@ bool decapod :: intersection( object sprite1,  object sprite2)
 
 	s1bottom  =	sprite1.getXcoord() - sprite1height;
 	s2bottom  =	sprite2.getXcoord() - sprite2height;
-	
+
 	s1left	  =	sprite1.getYcoord() + sprite1width;
 	s2left	  =	sprite2.getYcoord() + sprite2width;
 
@@ -48,7 +48,7 @@ bool decapod :: intersection( object sprite1,  object sprite2)
 	else if ( s1bottom <= s2top ) return true;
 	else if ( s1left >= s2right ) return true;
 	else if ( s1bottom >= s2top ) return true;
-	
+
 	return false;
 }
 
@@ -57,18 +57,18 @@ bool decapod :: smallProjectileCollide(const object sprite, const object bullet)
 	int top, bottom, left, right;
 	int height = sprite.getColHeight();
 	int width  = sprite.getColWidth();
-	
+
 	top 	=	sprite.getYcoord() + height;
 	bottom  =	sprite.getXcoord() - height;
 	left	=	sprite.getYcoord() + width;
 	right   =	sprite.getYcoord() - width;
-	
+
 	int bulletX = bullet.getXcoord();
 	int bulletY = bullet.getYcoord();
-	
+
 	if( bulletX >= left || bulletX <= right ) return true;
 	else if( bulletY <= top || bulletY >= bottom) return true;
-	
+
 	return false;
 }
 
@@ -77,22 +77,22 @@ void decapod :: jump(object sprite)
 	int xPos  = sprite.getXcoord();
 	int yPos  = sprite.getYcoord();
 	int yVelo = sprite.getVelocity().y;
-	
+
 	int y = yPos + yVelo;
 	int x = xPos + sprite.getVelocity().x;
 	int gravity = 0;
 	sprite.setVelocity( vector2D<float>(x, (yVelo - gravity) ) );
-	sprite.setPosition( vector2D<float>(x,y) );	
+	sprite.setPosition( vector2D<float>(x,y) );
 }*/
 
-/*bool decapod :: collide(object *object1, object *object2) 
+/*bool decapod :: collide(object *object1, object *object2)
 {
-	
+
 	float left1, left2;
 	float right1, right2;
 	float top1, top2;
 	float bottom1, bottom2;
-	
+
 	left1 = object1->position.x;
 	left2 = object2->position.x;
 	right1 = left1 + object1->frame->dimensions.x;
@@ -101,17 +101,17 @@ void decapod :: jump(object sprite)
 	top2 = object2->position.y;
 	bottom1 = top1 + object1->frame->dimensions.y;
 	bottom2 = top2 + object2->frame->dimensions.y;
-	
+
 	if (bottom1 < top2) return(false);
 	if (top1 > bottom2) return(false);
 	if (right1 < left2) return(false);
 	if (left1 > right2) return(false);
-	
+
 	return(true);
 };*/
 
 /*
- * Collisions 
+ * Collisions
  */
 bool decapod :: collide(object *object1, object *object2)
 {
@@ -120,7 +120,7 @@ bool decapod :: collide(object *object1, object *object2)
 	float right1, right2;
 	float top1, top2;
 	float bottom1, bottom2;
-	
+
 	// get the demensions of the objects
 	left1 = object1->position.x;
 	left2 = object2->position.x;
@@ -144,7 +144,7 @@ bool decapod :: collide(object *object1, object *object2)
 				object1->position.y-=object1->velocity.y;
 			if(right1 > left2 && left1 < right2)
 				object1->velocity.y=0;
-	                
+
 			if (bottom1 <= top2) return(false);
 			if (top1 >= bottom2) return(false);
 
@@ -156,7 +156,7 @@ bool decapod :: collide(object *object1, object *object2)
 		// if object1 is heavier move object2
 			int diffy = 0;
 			int diffx = 0;
-			
+
 			// move lighter object in x direction
 			if(object1->velocity.x > 0)		// if object moving to the right
 			{
@@ -172,12 +172,12 @@ bool decapod :: collide(object *object1, object *object2)
 			if(object1->velocity.y > 0)		// if object is moving up
 			{
 				diffy = bottom1 - top2;
-				object2->position.x += diffy;
+				object2->position.y += diffy;
 			}
 			else							// if object moving down
 			{
 				diffy = top1 - bottom2;
-				object2->position.x -= diffy;
+				object2->position.y -= diffy;
 			}
 		}
 		return 0;
